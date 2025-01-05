faa29c4616a60618fc0f91f9932223e0f98b37e8
shzyk <shzyk@foxmail.com>
2025-01-05 06:02:43 +0000

    提交

--- svm.py
+++ svm.py
@@ -130,36 +130,10 @@ if __name__ == '__main__':
     tsne = TSNE(n_components=2, random_state=seed)
     X_tsne = tsne.fit_transform(X_verify)
     plt.figure(figsize=(8, 6))
-    scatter = plt.scatter(X_tsne[:, 0], X_tsne[:, 1], c=Y_verify, cmap='viridis', alpha=0.7)
+    scatter = plt.scatter(X_tsne[:, 0], X_tsne[:, 1], c=Y_pred, cmap='viridis', alpha=0.7)
     plt.colorbar(scatter)
     plt.title("t-SNE Visualization of Validation Set Predictions")
     plt.xlabel("t-SNE Component 1")
     plt.ylabel("t-SNE Component 2")
     plt.savefig('tsne_visualization.png')  # 保存为PNG文件
     plt.close()  # 关闭图像以释放内存
-
-
-    ## 模型选择
-    kf = StratifiedKFold(n_splits=5, shuffle=True, random_state=seed)
-    accuracies = []
-    for train_index, verify_index in kf.split(X, Y):
-        X_train, X_verify = X[train_index], X[verify_index]
-        Y_train, Y_verify = Y[train_index], Y[verify_index]
-
-        scaler = MinMaxScaler()
-        X_train_scaled = scaler.fit_transform(X_train)
-        pca = PCA(n_components=0.95)  # 保留95%的方差
-        X_train_pca = pca.fit_transform(X_train_scaled)
-        print("降维后特征数量：", X_train_pca.shape[1])
-
-        model = MultiClassSVM(max_iter=args.max_iter)
-        model.fit(X_train_pca, Y_train)
-        X_verify_scaled = scaler.transform(X_verify)
-        X_verify_pca = pca.transform(X_verify_scaled)
-        Y_pred = model.predict(X_verify_pca)
-        accuracy = accuracy_score(Y_verify, Y_pred)*100
-        print("The Accuracy is " + str(accuracy),"%")
-        accuracies.append(accuracy)
-
-    # 输出平均准确率和标准差
-    print(f"The average accuracy is {np.mean(accuracies):.2f}%, with a standard deviation of {np.std(accuracies):.2f}%.")
